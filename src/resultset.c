f32a55298001e02be5e3e85295c72a719ad5597b
Vincent Rogier <vince.rogier@gmail.com>
2018-02-21 19:53:31 +0100

    fixed warning

--- src/resultset.c
+++ src/resultset.c
@@ -1331,7 +1331,7 @@ boolean OCI_API OCI_SetStructNumericType
     OCI_CALL_CHECK_COMPAT(rs->stmt->con, OCI_CDT_NUMERIC == rs->defs[index - 1].col.datatype);
     OCI_CALL_CONTEXT_SET_FROM_STMT(rs->stmt)
 
-    rs->defs[index-1].col.struct_subtype = type;
+    rs->defs[index-1].col.struct_subtype = (ub2) type;
 
     OCI_RETVAL = OCI_STATUS;
 
