4aadd1d98140e3862c24028ee049239d2989aa1e
Vincent Rogier <vince.rogier@gmail.com>
2020-04-17 19:03:15 +0200

    Updated copyrights to 2020

--- src/agent.c
+++ src/agent.c
@@ -3,7 +3,7 @@
  *
  * Website: http://www.ocilib.net
  *
- * Copyright (c) 2007-2019 Vincent ROGIER <vince.rogier@ocilib.net>
+ * Copyright (c) 2007-2020 Vincent ROGIER <vince.rogier@ocilib.net>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
