vrogier/ocilib

commit eb7e47b85a24f564b4fd483bc7315439b23f9de6

- Enhanced performance when fetching Clobs as strings (for Oracle 11g and above) - See issue #223 - When NOT using OCI_ENV_CONTEXT environment flag, OCI_CollToText() andOCI_ObjectToText() fail if previous OCILIB call failed - some performance improvements when converting Objects and Collections to strings - some performance improvements when calling OCI_GetString() on non strings columns

src/ocilib_defs.h
@@ -411,7 +411,7 @@
                                                                                 \
     if ((mode) & OCI_ENV_CONTEXT)                                               \
     {                                                                           \
-        ctx->call_err = OCI_ErrorGet(FALSE);                                    \
+        ctx->call_err = OCI_ErrorGet(FALSE, FALSE);                             \
         OCI_CallEnter(ctx);                                                     \
     }
 
@@ -438,9 +438,12 @@
     goto ExitCall;                                                              \
 
 #define OCI_CALL_CONTEXT_SET(c, s, e)                                           \
-    ctx->lib_con      = (c);                                                    \
-    ctx->lib_stmt     = (s);                                                    \
-    ctx->oci_err      = (e) ? (e) : OCILib.err;                                 \
+    if (OCILib.env_mode & OCI_ENV_CONTEXT)                                      \
+    {                                                                           \
+        ctx->lib_con      = (c);                                                \
+        ctx->lib_stmt     = (s);                                                \
+        ctx->oci_err      = (e) ? (e) : OCILib.err;                             \
+    }
 
 #define OCI_CALL_CONTEXT_SET_FROM_CONN(con)   OCI_CALL_CONTEXT_SET((con), NULL, ((con) ? (con)->err : OCILib.err))
 #define OCI_CALL_CONTEXT_SET_FROM_OBJ(obj)    OCI_CALL_CONTEXT_SET(((obj)->con), NULL, ((obj)->err))

include/ocilib.h
@@ -1807,6 +1807,7 @@ typedef unsigned int big_uint;
 
 #define OCI_SIZE_FORMAT                     64
 #define OCI_SIZE_BUFFER                     512
+#define OCI_SIZE_LARGE_BUFFER               ((64*1024)-1)
 #define OCI_SIZE_LONG                       ((64*1024)-1)
 #define OCI_SIZE_DATE                       45
 #define OCI_SIZE_TIMESTAMP                  54
@@ -1843,6 +1844,7 @@ typedef unsigned int big_uint;
 #define OCI_STRING_FALSE                    OTEXT("FALSE")
 #define OCI_STRING_TRUE_SIZE                4
 #define OCI_STRING_FALSE_SIZE               5
+#define OCI_STRING_NULL_SIZE                4
 
 #ifdef _WINDOWS
   #define OCI_CHAR_SLASH                    '\\'

src/collection.c
@@ -523,15 +523,15 @@ boolean OCI_API OCI_CollToText
     OCI_CALL_CHECK_PTR(OCI_IPC_VOID, size)
     OCI_CALL_CONTEXT_SET_FROM_CONN(coll->con)
 
-    err = OCI_ErrorGet(TRUE);
+    err = OCI_ErrorGet(TRUE, TRUE);
 
     if (str)
     {
         *str = 0;
     }
 
-    len += OCI_StringAddToBuffer(str, len, coll->typinf->name, FALSE);
-    len += OCI_StringAddToBuffer(str, len, OTEXT("("), FALSE);
+    len += OCI_StringAddToBuffer(str, len, coll->typinf->name, (unsigned int) ostrlen(coll->typinf->name), FALSE);
+    len += OCI_StringAddToBuffer(str, len, OTEXT("("), 1, FALSE);
 
     const int n = OCI_CollGetSize(coll);
 
@@ -543,7 +543,7 @@ boolean OCI_API OCI_CollToText
 
         if (OCI_ElemIsNull(elem))
         {
-            len += OCI_StringAddToBuffer(str, len, OCI_STRING_NULL, FALSE);
+            len += OCI_StringAddToBuffer(str, len, OCI_STRING_NULL, OCI_STRING_NULL_SIZE, FALSE);
         }
         else
         {
@@ -555,7 +555,8 @@ boolean OCI_API OCI_CollToText
             {
                 case OCI_CDT_TEXT:
                 {
-                    data  = (void *) OCI_ElemGetString(elem);
+                    data_size = OCIStringSize(OCILib.env, elem->handle);
+                    data      = (void *) OCI_ElemGetString(elem);
                     break;
                 }
                 case OCI_CDT_NUMERIC:
@@ -640,7 +641,7 @@ boolean OCI_API OCI_CollToText
                 }
                 else
                 {
-                    len += OCI_StringAddToBuffer(str, len, OCI_STRING_NULL, FALSE);
+                    len += OCI_StringAddToBuffer(str, len, OCI_STRING_NULL, OCI_STRING_NULL_SIZE, FALSE);
                 }
                 OCI_STATUS = (NULL == err || OCI_UNKNOWN == err->type);
             }
@@ -648,13 +649,13 @@ boolean OCI_API OCI_CollToText
 
         if (OCI_STATUS && i < n)
         {
-            len += OCI_StringAddToBuffer(str, len, OTEXT(", "), FALSE);
+            len += OCI_StringAddToBuffer(str, len, OTEXT(", "), 2, FALSE);
         }
     }
 
     if (OCI_STATUS)
     {
-        len += OCI_StringAddToBuffer(str, len, OTEXT(")"), FALSE);
+        len += OCI_StringAddToBuffer(str, len, OTEXT(")"), 1, FALSE);
 
         *size = len;
     }

src/connection.c
@@ -852,7 +852,7 @@ boolean OCI_ConnectionClose
 
     /* clear connection reference from current error object */
 
-    err = OCI_ErrorGet(FALSE);
+    err = OCI_ErrorGet(FALSE, FALSE);
 
     if (err && err->con == con)
     {

src/define.c
@@ -368,5 +368,19 @@ boolean OCI_DefineDef
         }
     }
 
+#if OCI_VERSION_COMPILE >= OCI_11_1
+
+    if (OCI_ConnectionIsVersionSupported(def->rs->stmt->con, OCI_11_1))
+    {
+        if (OCI_CDT_LOB == def->col.datatype)
+        {
+            ub2 value = 1;
+
+            OCI_SET_ATTRIB(OCI_HTYPE_DEFINE, OCI_ATTR_LOBPREFETCH_LENGTH, def->buf.handle, &value, sizeof(value))
+        }
+    }
+
+#endif
+
     return OCI_STATUS;
 }

src/error.c
@@ -84,7 +84,8 @@ void OCI_ErrorReset
 
 OCI_Error * OCI_ErrorGet
 (
-    boolean check
+    boolean check,
+    boolean reset
 )
 {
     OCI_Error *err = NULL;
@@ -112,6 +113,12 @@ OCI_Error * OCI_ErrorGet
     if (check && err && err->active)
     {
         err = NULL;
+    }
+
+    // Reset error in case OCI_ENV_CONTEXT is no used
+    if (reset && err && err->depth == 0 && err->type != OCI_UNKNOWN)
+    {
+        OCI_ErrorReset(err);
     }
 
     return err;

src/exception.c
@@ -228,7 +228,7 @@ OCI_Error * OCI_ExceptionGetError
     void
 )
 {
-    OCI_Error *err = OCI_ErrorGet(TRUE);
+    OCI_Error *err = OCI_ErrorGet(TRUE, FALSE);
 
     if (err)
     {

src/library.c
@@ -1767,7 +1767,7 @@ boolean OCI_API OCI_Cleanup
     if (OCILib.key_errs)
     {        
         OCI_ThreadKey *key = OCILib.key_errs;
-        OCI_Error     *err = OCI_ErrorGet(FALSE);
+        OCI_Error     *err = OCI_ErrorGet(FALSE, FALSE);
 
         OCILib.key_errs = NULL;
 
@@ -1921,7 +1921,7 @@ OCI_Error * OCI_API OCI_GetLastError
 
     if (!OCILib.loaded || OCILib.env_mode & OCI_ENV_CONTEXT)
     {
-        err = OCI_ErrorGet(TRUE);
+        err = OCI_ErrorGet(TRUE, FALSE);
 
         if (err && (!err->raise))
         {

commit d66e8a98012ccd5114f2ee26129b542594ffdd02

Fixed a regression introduced in commit eb7e47b8

src/ocilib_defs.h
@@ -438,12 +438,9 @@
     goto ExitCall;                                                              \
 
 #define OCI_CALL_CONTEXT_SET(c, s, e)                                           \
-    if (OCILib.env_mode & OCI_ENV_CONTEXT)                                      \
-    {                                                                           \
-        ctx->lib_con      = (c);                                                \
-        ctx->lib_stmt     = (s);                                                \
-        ctx->oci_err      = (e) ? (e) : OCILib.err;                             \
-    }
+    ctx->lib_con      = (c);                                                    \
+    ctx->lib_stmt     = (s);                                                    \
+    ctx->oci_err      = (e) ? (e) : OCILib.err;                                 \
 
 #define OCI_CALL_CONTEXT_SET_FROM_CONN(con)   OCI_CALL_CONTEXT_SET((con), NULL, ((con) ? (con)->err : OCILib.err))
 #define OCI_CALL_CONTEXT_SET_FROM_OBJ(obj)    OCI_CALL_CONTEXT_SET(((obj)->con), NULL, ((obj)->err))