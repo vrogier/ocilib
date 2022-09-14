a642ec210bf6d87d0cb781f71be55058698cba07
Vincent Rogier <vince.rogier@ocilib.net>
2022-09-14 10:46:08 +0200

    Adding more test cases for issue #309

--- tests/TestREportedIssuesCppApi.cpp
+++ tests/TestREportedIssuesCppApi.cpp
@@ -35,7 +35,7 @@ TEST(ReportedIssuesCppApi, Issue250)
 
 TEST(ReportedIssuesCppApi, Issue309_NoDataFoundPlsqlWithError)
 {
-    ExecDML(OTEXT("create table TestTableIssue309(code number, name varchar2(50))"));
+    ExecDML(OTEXT("create table TestTableIssue309_1(code number, name varchar2(50))"));
     
     int errCode{};
 
@@ -53,7 +53,7 @@ TEST(ReportedIssuesCppApi, Issue309_NoDataFoundPlsqlWithError)
             declare
                 result varchar2(50);
             begin
-                select name into result from TestTableIssue309 where code = 1;
+                select name into result from TestTableIssue309_1 where code = 1;
             end;
             )"
         );
@@ -69,12 +69,69 @@ TEST(ReportedIssuesCppApi, Issue309_NoDataFoundPlsqlWithError)
 
     Environment::Cleanup();
 
-    ExecDML(OTEXT("drop table TestTableIssue309"));
+    ExecDML(OTEXT("drop table TestTableIssue309_1"));
+}
+
+TEST(ReportedIssuesCppApi, Issue309_NoDataFoundSqlCallingPlsqlWithoutError)
+{
+    ExecDML(OTEXT("create table TestTableIssue309_2(code number)"));
+    ExecDML(OTEXT(
+        R"(
+            CREATE OR REPLACE PACKAGE TestPackageIssue309 IS
+                FUNCTION ReturnNumber return number;
+            END;
+            )"
+        ));
+    ExecDML(OTEXT(
+        R"(
+            CREATE OR REPLACE PACKAGE BODY TestPackageIssue309 IS
+                FUNCTION ReturnNumber return number
+	            AS
+		            result number;
+	            BEGIN
+	                select code into result from TestTableIssue309_2 where code = 1;
+	                return result;
+	            END;
+            END;
+            )"
+    ));
+
+    auto exceptionOccured{ false };
+    auto counter{ 0 };
+    auto isNull{ false };
+    try
+    {
+        ocilib::Environment::Initialize();
+
+        Connection con(DBS, USR, PWD);
+        ocilib::Statement stmt(con);
+
+        stmt.Execute(OTEXT("select TestPackageIssue309.ReturnNumber() from dual"));
+        auto resultset = stmt.GetResultset();
+        while (resultset.Next())
+        {
+            isNull = resultset.IsColumnNull(1);
+            counter++;
+        }
+    }
+    catch (ocilib::Exception e)
+    {
+        exceptionOccured = true;
+    }
+
+    ASSERT_EQ(1, counter);
+    ASSERT_FALSE(exceptionOccured);
+    ASSERT_TRUE(isNull);
+
+    Environment::Cleanup();
+
+    ExecDML(OTEXT("drop package TestPackageIssue309"));
+    ExecDML(OTEXT("drop table TestTableIssue309_2"));
 }
 
 TEST(ReportedIssuesCppApi, Issue309_NoDataFoundSqlWithoutError)
 {
-    ExecDML(OTEXT("create table TestTableIssue309(code number, name varchar2(50))"));
+    ExecDML(OTEXT("create table TestTableIssue309_3(code number, name varchar2(50))"));
 
     auto exceptionOccured{ false };
     auto counter{ 0 };
@@ -85,7 +142,7 @@ TEST(ReportedIssuesCppApi, Issue309_NoDataFoundSqlWithoutError)
         Connection con(DBS, USR, PWD);
         ocilib::Statement stmt(con);
 
-        stmt.Execute(OTEXT(" select code, name from TestTableIssue309"));
+        stmt.Execute(OTEXT(" select code, name from TestTableIssue309_3"));
         auto resultset = stmt.GetResultset();
         while (resultset.Next())
         {
@@ -102,7 +159,7 @@ TEST(ReportedIssuesCppApi, Issue309_NoDataFoundSqlWithoutError)
 
     Environment::Cleanup();
 
-    ExecDML(OTEXT("drop table TestTableIssue309"));
+    ExecDML(OTEXT("drop table TestTableIssue309_3"));
 }
 
 TEST(ReportedIssuesCppApi, Issue314)
