10e445faf683856e28d7e3e90bb3cfdabac2d8c5
Vincent Rogier <vince.rogier@ocilib.net>
2022-09-16 13:47:54 +0200

    updated copyright to 2022

--- demo/ocilib_demo.cpp
+++ demo/ocilib_demo.cpp
@@ -3,7 +3,7 @@
 *
 * Website: http://www.ocilib.net
 *
-* Copyright (c) 2007-2021 Vincent ROGIER <vince.rogier@ocilib.net>
+* Copyright (c) 2007-2022 Vincent ROGIER <vince.rogier@ocilib.net>
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
