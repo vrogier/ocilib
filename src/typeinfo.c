vrogier/ocilib

commit 279bf79a0bc2f5e4e4bb791a678ef1fda6205159

Applied change previously made for issue #297 to other places Refactored list macros and simplied code usage

src/typeinfo.c
@@ -61,9 +61,11 @@ static boolean OcilibTypeInfoFind(OCI_TypeInfo *typinf, TypeInfoFindParams *find
 
 static OCI_TypeInfo* OcilibTypeInfoFindOrCreate(OCI_Connection* con, TypeInfoFindParams* p_find_params, boolean* p_is_created)
 {
-    *p_is_created = FALSE;
+    OCI_TypeInfo* typinf = NULL;
 
-    OCI_TypeInfo* typinf = OcilibListFind(con->tinfs, (POCI_LIST_FIND)OcilibTypeInfoFind, p_find_params);
+    *p_is_created = FALSE;
+    
+    LIST_ATOMIC_FIND(con->tinfs, OcilibTypeInfoFind, p_find_params, typinf)
 
     if (NULL == typinf)
     {
@@ -233,7 +235,7 @@ OCI_TypeInfo * OcilibTypeInfoGet
     find_params.name   = obj_name;
     find_params.schema = obj_schema;
 
-    LOCK_LIST
+    LIST_ATOMIC_OPERATION
     (
         con->tinfs,
         {

src/array.c
@@ -223,13 +223,7 @@ OCI_Array * OcilibArrayCreate
 
     /* create array object */
 
-    LOCK_LIST
-    (
-        Env.arrs,
-        {
-            arr = OcilibListAppend(Env.arrs, sizeof(*arr));
-        }
-    )
+    LIST_ATOMIC_ADD(Env.arrs, arr)
 
     CHECK_NULL(arr)
 
@@ -304,21 +298,7 @@ boolean OcilibArrayFreeFromHandles
 
     CHECK_PTR(OCI_IPC_VOID, handles)
 
-    WARNING_DISABLE_UNSAFE_CONVERT
-
-    LOCK_LIST
-    (
-        Env.arrs,
-        {
-            arr = OcilibListFind(Env.arrs, (POCI_LIST_FIND)OcilibArrayFindAny, (void *) handles);
-            if (NULL != arr)
-            {
-                OcilibListRemove(Env.arrs, arr);
-            }
-        }
-    )
-
-    WARNING_RESTORE_UNSAFE_CONVERT
+    LIST_ATOMIC_FIND_AND_REMOVE(Env.arrs, OcilibArrayFindAny, handles, arr)
             
     CHECK_NULL(arr)
 

src/callback.c
@@ -632,13 +632,7 @@ void OcilibCallbackHAEvent
 
             /* notify all related connections */
 
-            LOCK_LIST
-            (
-                Env.cons,
-                {
-                    OcilibListForEachWithParam(Env.cons, &params, (POCI_LIST_FOR_EACH_WITH_PARAM)OcilibProcHAEventInvoke);
-                }
-            )
+            LIST_ATOMIC_FOREACH_WITH_PARAM(Env.cons, &params, OcilibProcHAEventInvoke)
 
             /* get next server */
 

src/connection.c
@@ -114,13 +114,7 @@ OCI_Connection * OcilibConnectionAllocate
 
     OCI_Connection* con = NULL;
 
-    LOCK_LIST
-    (
-        Env.cons,
-        {
-            con = OcilibListAppend(Env.cons, sizeof(*con));
-        }
-    )
+    LIST_ATOMIC_ADD(Env.cons, con)
 
     CHECK_NULL(con)
 
@@ -967,50 +961,14 @@ static boolean OcilibConnectionLogOff
     }
 
     /* dissociate connection from existing subscriptions */
-    LOCK_LIST
-    (
-        Env.subs,
-        {
-            OcilibListForEachWithParam(Env.subs, con, (POCI_LIST_FOR_EACH_WITH_PARAM)OcilibConnectionDetachSubscriptions); 
-        }
-    )
 
-    WARNING_DISABLE_CAST_FUNC_TYPE
+    LIST_ATOMIC_FOREACH_WITH_PARAM(Env.subs, con, OcilibConnectionDetachSubscriptions)
 
-    /* free all statements */
+    /* free all child lists */
 
-    LOCK_LIST
-    (
-        con->stmts, 
-        {
-            OcilibListForEach(con->stmts, (POCI_LIST_FOR_EACH)OcilibStatementDispose);
-            OcilibListClear(con->stmts);
-        }
-    )
-
-    /* free all type info objects */
-
-    LOCK_LIST
-    (
-        con->tinfs,
-        {
-            OcilibListForEach(con->tinfs, (POCI_LIST_FOR_EACH)OcilibTypeInfoDispose);
-            OcilibListClear(con->tinfs);
-        }
-    )
-
-    /* free all transactions */
-
-    LOCK_LIST
-    (
-        con->trsns,
-        {
-            OcilibListForEach(con->trsns, (POCI_LIST_FOR_EACH)OcilibTransactionDispose);
-            OcilibListClear(con->trsns);
-        }
-    )
-
-    WARNING_RESTORE_CAST_FUNC_TYPE
+    LIST_ATOMIC_REMOVE_ALL(con->stmts, OcilibStatementDispose)
+    LIST_ATOMIC_REMOVE_ALL(con->tinfs, OcilibTypeInfoDispose)
+    LIST_ATOMIC_REMOVE_ALL(con->trsns, OcilibTransactionDispose)
 
     /* 1 - XA Connection */
 
@@ -1243,15 +1201,7 @@ boolean OcilibConnectionFree
 
     CHECK_PTR(OCI_IPC_CONNECTION, con)
 
-    LOCK_LIST
-    (
-        Env.cons,
-        {
-            OcilibListRemove(Env.cons, con);
-        }
-    )
-
-    CHECK(OcilibConnectionDispose(con))
+    LIST_ATOMIC_REMOVE(Env.cons, con, OcilibConnectionDispose)
 
     FREE(con)
 

src/environment.c
@@ -1530,41 +1530,10 @@ boolean OcilibEnvironmentCleanup
 
     /* dispose list items */
 
-    LOCK_LIST
-    (
-        Env.arrs,
-        {
-            OcilibListForEach(Env.arrs,  (POCI_LIST_FOR_EACH)OcilibArrayDispose);
-            OcilibListClear(Env.arrs);
-        }
-    )
-
-    LOCK_LIST
-    (
-        Env.subs,
-        {
-            OcilibListForEach(Env.subs,  (POCI_LIST_FOR_EACH)OcilibSubscriptionDispose);
-            OcilibListClear(Env.subs);
-        }
-    )
-
-    LOCK_LIST
-    (
-        Env.cons,
-        {
-            OcilibListForEach(Env.cons,  (POCI_LIST_FOR_EACH)OcilibConnectionDispose);
-            OcilibListClear(Env.cons);
-        }
-    )
-
-    LOCK_LIST
-    (
-        Env.pools,
-        {
-            OcilibListForEach(Env.pools, (POCI_LIST_FOR_EACH)OcilibPoolDispose);
-            OcilibListClear(Env.pools);
-        }
-    )
+    LIST_ATOMIC_REMOVE_ALL(Env.arrs,  OcilibArrayDispose)
+    LIST_ATOMIC_REMOVE_ALL(Env.subs,  OcilibSubscriptionDispose)
+    LIST_ATOMIC_REMOVE_ALL(Env.cons,  OcilibConnectionDispose)
+    LIST_ATOMIC_REMOVE_ALL(Env.pools, OcilibPoolDispose)
 
     /* free all lists */
 

src/macros.h
@@ -26,44 +26,44 @@
 #include "memory.h"
 #include "types.h"
 
-/* Global warning macro specific warnings macro */
+ /* Global warning macro specific warnings macro */
 
 #if defined(_MSC_VER)
 
-    #define WARNING_DISABLE(w)              __pragma(warning( disable : w ))
-    #define WARNING_RESTORE(w)              __pragma(warning( default : w ))
+#define WARNING_DISABLE(w)              __pragma(warning( disable : w ))
+#define WARNING_RESTORE(w)              __pragma(warning( default : w ))
 
-    #define WARNING_DISABLE_CAST_FUNC_TYPE  WARNING_DISABLE(4054)
-    #define WARNING_RESTORE_CAST_FUNC_TYPE  WARNING_RESTORE(4054)
-    #define WARNING_DISABLE_PEDANTIC
-    #define WARNING_RESTORE_PEDANTIC
-    #define WARNING_DISABLE_UNSAFE_CONVERT  WARNING_DISABLE(4191)
-    #define WARNING_RESTORE_UNSAFE_CONVERT  WARNING_RESTORE(4191)
+#define WARNING_DISABLE_CAST_FUNC_TYPE  WARNING_DISABLE(4054)
+#define WARNING_RESTORE_CAST_FUNC_TYPE  WARNING_RESTORE(4054)
+#define WARNING_DISABLE_PEDANTIC
+#define WARNING_RESTORE_PEDANTIC
+#define WARNING_DISABLE_UNSAFE_CONVERT  WARNING_DISABLE(4191)
+#define WARNING_RESTORE_UNSAFE_CONVERT  WARNING_RESTORE(4191)
 
 #elif defined(__GNUC__)
 
-    #define DO_PRAGMA(exp) _Pragma(#exp)
+#define DO_PRAGMA(exp) _Pragma(#exp)
 
-    #define WARNING_DISABLE(w)                  \
+#define WARNING_DISABLE(w)                  \
         DO_PRAGMA(GCC diagnostic push)          \
         DO_PRAGMA(GCC diagnostic ignored #w)
 
-    #define WARNING_RESTORE(w)                  \
+#define WARNING_RESTORE(w)                  \
         DO_PRAGMA(GCC diagnostic pop)           \
 
-	#if __GNUC__ > 7
-      #define WARNING_DISABLE_CAST_FUNC_TYPE      WARNING_DISABLE(-Wcast-function-type)
-      #define WARNING_RESTORE_CAST_FUNC_TYPE      WARNING_RESTORE(-Wcast-function-type)
-    #else
-      #define WARNING_DISABLE_CAST_FUNC_TYPE
-      #define WARNING_RESTORE_CAST_FUNC_TYPE
-	#endif
+#if __GNUC__ > 7
+#define WARNING_DISABLE_CAST_FUNC_TYPE      WARNING_DISABLE(-Wcast-function-type)
+#define WARNING_RESTORE_CAST_FUNC_TYPE      WARNING_RESTORE(-Wcast-function-type)
+#else
+#define WARNING_DISABLE_CAST_FUNC_TYPE
+#define WARNING_RESTORE_CAST_FUNC_TYPE
+#endif
 
-	#define WARNING_DISABLE_PEDANTIC            WARNING_DISABLE(-Wpedantic)
-    #define WARNING_RESTORE_PEDANTIC            WARNING_RESTORE(-Wpedantic)
-  
-    #define WARNING_DISABLE_UNSAFE_CONVERT
-    #define WARNING_RESTORE_UNSAFE_CONVERT
+#define WARNING_DISABLE_PEDANTIC            WARNING_DISABLE(-Wpedantic)
+#define WARNING_RESTORE_PEDANTIC            WARNING_RESTORE(-Wpedantic)
+
+#define WARNING_DISABLE_UNSAFE_CONVERT
+#define WARNING_RESTORE_UNSAFE_CONVERT
 
 #endif
 
@@ -479,11 +479,102 @@ ExitLabel:                          \
         (ub4) (size), (ub4) (atype), err                                   \
     )       
 
+#define LIST_ATOMIC_OPERATION(list, exp)                    \
+                                                            \
+    OcilibListLock(list);                                   \
+    WARNING_DISABLE_UNSAFE_CONVERT                          \
+    exp                                                     \
+    WARNING_RESTORE_UNSAFE_CONVERT                          \
+    OcilibListUnlock(list);                                 \
 
-#define LOCK_LIST(list, exp)    \
-                                \
-    OcilibListLock(list);       \
-    exp                         \
-    OcilibListUnlock(list);     \
+#define LIST_ATOMIC_FOREACH(list, cb)                       \
+                                                            \
+    LIST_ATOMIC_OPERATION                                   \
+    (                                                       \
+        list,                                               \
+        {                                                   \
+            OcilibListForEach(list, (POCI_LIST_FOR_EACH)cb);\
+        }                                                   \
+    ) 
+
+#define LIST_ATOMIC_FOREACH_WITH_PARAM(list, param, cb)     \
+                                                            \
+    LIST_ATOMIC_OPERATION                                   \
+    (                                                       \
+        list,                                               \
+        {                                                   \
+            OcilibListForEachWithParam                      \
+            (                                               \
+                list,(void *) param,                        \
+                (POCI_LIST_FOR_EACH_WITH_PARAM)cb           \
+            );                                              \
+        }                                                   \
+    ) 
+
+#define LIST_ATOMIC_REMOVE_ALL(list, cb)                    \
+                                                            \
+    LIST_ATOMIC_OPERATION                                   \
+    (                                                       \
+        list,                                               \
+        {                                                   \
+            OcilibListForEach(list, (POCI_LIST_FOR_EACH)cb);\
+            OcilibListClear(list);                          \
+        }                                                   \
+    )                                                       \
+
+#define LIST_ATOMIC_FIND(list, cb, data, item)              \
+                                                            \
+    LIST_ATOMIC_OPERATION                                   \
+    (                                                       \
+        list,                                               \
+        {                                                   \
+            item = OcilibListFind                           \
+            (                                               \
+                list, (POCI_LIST_FIND) cb,                  \
+                (void *) data                               \
+            );                                              \
+        }                                                   \
+    )                                                       \
+
+#define LIST_ATOMIC_ADD(list, item)                         \
+                                                            \
+    LIST_ATOMIC_OPERATION                                   \
+    (                                                       \
+        list,                                               \
+        {                                                   \
+            item = OcilibListAppend(list, sizeof(*item));   \
+        }                                                   \
+    )                                                       \
+
+#define LIST_ATOMIC_REMOVE(list, item, cb)                  \
+                                                            \
+    LIST_ATOMIC_OPERATION                                   \
+    (                                                       \
+        list,                                               \
+        {                                                   \
+            OcilibListRemove(list, item);                   \
+        }                                                   \
+    )                                                       \
+    cb(item);                                               \
+
+
+#define LIST_ATOMIC_FIND_AND_REMOVE(list, cb, data, item)   \
+                                                            \
+    LIST_ATOMIC_OPERATION                                   \
+    (                                                       \
+        list,                                               \
+        {                                                   \
+            item = OcilibListFind                           \
+            (                                               \
+                list, (POCI_LIST_FIND) cb,                  \
+                (void *) data                               \
+            );                                              \
+            if (NULL != item)                               \
+            {                                               \
+                OcilibListRemove(list, item);               \
+            }                                               \
+        }                                                   \
+    )                                                       \
+
 
 #endif /* OCILIB_MACROS_H_INCLUDED */

src/object.c
@@ -202,13 +202,7 @@ static OCI_TypeInfo * ObjectGetRealTypeInfo(OCI_TypeInfo *typinf, void *object)
 
     /* first try to find it in list */
 
-    LOCK_LIST
-    (
-        typinf->con->tinfs,
-        {
-            parent = OcilibListFind(typinf->con->tinfs, (POCI_LIST_FIND)OcilibObjectTypeInfoFind, tdo);
-        }
-    )
+    LIST_ATOMIC_FIND(typinf->con->tinfs, OcilibObjectTypeInfoFind, tdo, parent)
 
     /* if found, it will be assigned in the cleanup part */
 

src/pool.c
@@ -169,13 +169,7 @@ OCI_Pool * OcilibPoolCreate
 
     /* create pool object */
 
-    LOCK_LIST
-    (
-        Env.pools,
-        {
-            pool = OcilibListAppend(Env.pools, sizeof(*pool));
-        }
-    )
+    LIST_ATOMIC_ADD(Env.pools, pool)
 
     CHECK_NULL(pool)
 
@@ -395,15 +389,7 @@ boolean OcilibPoolFree
 
     CHECK_PTR(OCI_IPC_POOL, pool)
 
-    LOCK_LIST
-    (
-        Env.pools,
-        {
-            OcilibListRemove(Env.pools, pool);
-        }
-    )
-
-    CHECK(OcilibPoolDispose(pool))
+    LIST_ATOMIC_REMOVE(Env.pools, pool, OcilibPoolDispose)
 
     FREE(pool)
 

commit db231f7f9e7c239edcd23fd11edf373f4c601ce6

Fixed reentrance issue (deadlock)  caused by commit 279bf79a

src/typeinfo.c
@@ -61,12 +61,10 @@ static boolean OcilibTypeInfoFind(OCI_TypeInfo *typinf, TypeInfoFindParams *find
 
 static OCI_TypeInfo* OcilibTypeInfoFindOrCreate(OCI_Connection* con, TypeInfoFindParams* p_find_params, boolean* p_is_created)
 {
-    OCI_TypeInfo* typinf = NULL;
+    OCI_TypeInfo* typinf = OcilibListFind(con->tinfs, OcilibTypeInfoFind, p_find_params);
 
     *p_is_created = FALSE;
     
-    LIST_ATOMIC_FIND(con->tinfs, OcilibTypeInfoFind, p_find_params, typinf)
-
     if (NULL == typinf)
     {
         typinf = OcilibListAppend(con->tinfs, sizeof(OCI_TypeInfo));